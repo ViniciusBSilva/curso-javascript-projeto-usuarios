6e6ca2d912fa94bc5cf9f2245652ace3f476e45b
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2021-11-26 02:58:00 -0300

    g22 - handling buttons, edit/create form and form data

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,8 +6,19 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
+        this.onEdit();
 
-    }   // constructor (formId, tableId) 
+    } // constructor (formId, tableId) 
+
+    onEdit() {
+
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e => {
+
+            this.showPanelCreate();
+
+        });
+
+    } // onEdit
 
     onSubmit() {
 
@@ -49,7 +60,7 @@ class UserController {
 
         });
 
-    }   // onSubmit()
+    } // onSubmit()
 
     getPhoto() {
 
@@ -87,7 +98,7 @@ class UserController {
 
         });
 
-    }   // getPhoto()
+    } // getPhoto()
 
 
     getValues() {
@@ -143,7 +154,7 @@ class UserController {
             user.admin
         );
 
-    }   // getValues()
+    } // getValues()
 
     addLine(dataUser) {
 
@@ -159,17 +170,54 @@ class UserController {
                 <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
         `;
 
+        tr.querySelector(".btn-edit").addEventListener("click", e => {
+
+            let json = JSON.parse(tr.dataset.user);
+            let form = document.querySelector("#form-user-update");
+
+            for (let name in json) {
+
+                let field = form.querySelector("[name=" + name.replace("_", "") + "]");
+
+                if (field) {
+
+                    if (field.type == 'file') continue;
+
+                    field.value = json[name];
+
+                }
+
+            }
+
+            this.showPanelUpdate();
+
+        });
+
         this.tableEl.appendChild(tr);
 
         this.updateCount();
 
-    }   // addLine(dataUser)
+    } // addLine(dataUser)
+
+    showPanelCreate() {
+
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+
+    } // showPanelCreate()
+
+    showPanelUpdate() {
+
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
+
+    } // showPanelUpdate()
 
     updateCount() {
 
@@ -189,6 +237,6 @@ class UserController {
         document.querySelector("#number-users").innerHTML = numberUsers;
         document.querySelector("#number-users-admin").innerHTML = numberAdmin;
 
-    }   // updateCount()
+    } // updateCount()
 
-}   // class UserController 
+} // class UserController 
